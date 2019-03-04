7262a01eaf05cfd71e09cfc9c9d827e2c7e86965
daaviidmartinez <44229292+daaviidmartinez@users.noreply.github.com>
2019-03-04 09:18:09 +0100

    Create script5.sh
    Ejer5

---
+++ script5.sh
@@ -0,0 +1,16 @@
+echo "Escribe un numero"
+read numero
+
+suma=0
+
+while [ $numero != 0 ];do
+	suma=$((suma + numero))
+	contador=$((contador + 1))
+	media=$((suma / contador))
+	echo "Introduce otro número"
+        read numero
+
+done
+
+echo "La suma de todos los números es igual a $suma"
+echo "La media de todos los números es igual a $media"
